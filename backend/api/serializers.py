60b424e4c520c34002900b300b398732837482bc
Arbeid <arbeid@Andreass-MacBook-Pro.local>
2022-07-17 21:44:26 +0200

    add backend django with basic config

---
+++ backend/api/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from todo.models import Todo
+
+# ModelSerializer provides an API to create serializers from your models
+class TodoSerializer(serializers.ModelSerializer):
+    # auto populated by app. Users cannot be edited when it is fixed
+    created = serializers.ReadOnlyField()   
+    completed = serializers.ReadOnlyField()  
+
+    class Meta:
+        model = Todo
+        fields = ['id','title','memo','created','completed']
+
+class TodoToggleCompleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ['id'] 
+        read_only_fields = ['title','memo','created','completed']
+
+
+class TodoToggleCompleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ['id'] # why need to show id?
+        read_only_fields = ['title','memo','created','completed']
